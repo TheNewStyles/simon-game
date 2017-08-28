d5d811036dee8ca7a247cee27c11323d25a644b5
TheNewStyles <tyler.peterson@utah.edu>
2017-08-28 06:57:44 -0600

    Add sudo code. Create random number variable. TODO implement glow of square based on random number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    ngOnInit(){
+      console.log('here I am');
+    }
+
+    startGame(event) {
+      var randomNum = this.getRandomNumber(4);
+      console.log(randomNum);
+      //create random number between 0-3
+      //match up that number to a color and light up the color square in ui
+      //increment step count - pattern count 
+      //save random pattern in variable
+      //allow user to match pattern - save user pattern in variable
+      //compare values
+      //if values === continue - increment step count - increment pattern count
+        //else reset pattern count - step count remains (strict will be different)
+      
+    }
+
+    public getRandomNumber(max:number) {
+      return Math.floor(Math.random() * max) + 1;
+    }
 }
