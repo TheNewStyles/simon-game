05841684fdedbf50da92a740a4b93b607e67dfbb
TheNewStyles <tyler.peterson@utah.edu>
2017-08-28 19:28:13 -0600

    Add glow function for first random sequence and push to array TODO make glow only last for a short time. Allow user to match pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    ngOnInit(){
-      console.log('here I am');
-    }
+export class AppComponent {  
 
-    startGame(event) {
-      var randomNum = this.getRandomNumber(4);
-      console.log(randomNum);
+    private startGame(event) {
+      var stepCount = 0;
       //create random number between 0-3
+      var randomNum = this.getRandomNumber(4);
       //match up that number to a color and light up the color square in ui
+      this.glow(randomNum);
+      //save random num in array
+      var randomPattern = [];
+      randomPattern.push(randomNum);      
       //increment step count - pattern count 
-      //save random pattern in variable
+      stepCount++;
+
       //allow user to match pattern - save user pattern in variable
       //compare values
       //if values === continue - increment step count - increment pattern count
@@ -24,7 +26,29 @@ export class AppComponent {
       
     }
 
-    public getRandomNumber(max:number) {
+    private getRandomNumber(max:number) {
       return Math.floor(Math.random() * max) + 1;
+    }
+
+    private glow(randomNum:number) {
+      var color = '';
+      var element;
+
+      switch (randomNum) {
+        case 1:
+          color = 'green';
+          break;
+        case 2:
+          color = 'red';
+          break;
+        case 3:
+          color = 'yellow';
+          break;
+        case 4:
+          color = 'blue';
+          break;
+      }
+      element = window.document.querySelector('#' + color)
+      element.className = 'item ' + color + '-glow'; 
     }
 }
